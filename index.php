df9488cb5a0a27e76021b269300da120e0ed5da2
Spihelga <Sander.pihelgas@itcollege.ee>
2017-03-12 19:43:43 +0200

    nadal4

--- index.php
+++ index.php
@@ -47,6 +47,9 @@ The word "hand" in this context is defined as a combination of ingredients. The 
  <a href="http://validator.w3.org/check?uri=referer">
   <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
  </a>
+ <a href="http://jigsaw.w3.org/css-validator/check/referer">
+  <img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" />
+</a>
 </p>
 
 	<script src="js.js"></script>
